6d06b1496c0f3b7d8c877a0aff7432a15a179185
talha-rehman506 <talha@centumlogics.com>
2026-03-12 15:11:17 +0500

    update the welcome file with chat application

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ChatController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/chat', [ChatController::class, 'send']);
